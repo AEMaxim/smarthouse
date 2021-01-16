df2dac843c4b01b24404e7af9edda166d2e6ab12
AEMaxim <45178331+AEMaxim@users.noreply.github.com>
2021-01-16 09:33:36 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.min').addEventListener('click', () => {
+  document.querySelector('.min').classList.toggle('min-menu');
+  document.querySelector('.menu').classList.toggle('min-menu');
+});
